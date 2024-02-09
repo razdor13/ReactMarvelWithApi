7ab41cb3135aaed070c3e1812732470e013a6e03
Razdor13 <oneamaday@gmail.com>
2024-02-09 23:26:43 +0200

    rework charlist and app component as fucntion component and use hooks

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,101 +1,92 @@
-import {Component} from 'react';
+import {useEffect,useState,useRef} from 'react';
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMassage/ErrorMassage';
 import MarvelService from '../../services/MarvelService';
 import './charList.scss';
 
-class CharList extends Component {
-    state = {
-        charList: [],
-        loading: true,
-        error: false,
-        newItemLoading : false,
-        offset : 1550,
-        charEnded : false
+const CharList = (props) => {
+
+    const [charList, setCharList] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [newItemLoading, setNewItemLoading] = useState(false);
+    const [offset, setOffset] = useState(210);
+    const [charEnded, setCharEnded] = useState(false);
+    
+    const marvelService = new MarvelService();
+
+    useEffect(() => {
+        onRequest()
+    },[])//useEffect з пустим масивом виконається (ініціалізується лише один раз)(заміна componentDidMount)
+        // також не дивлячись на те що ми запускаємо цю функцію  onRequest() перед ініцаілізацію
+        //вона стрілочна, суть в тому що useEffect всеодно запуститься після рендеру .
+
+
+    const onRequest = (offset) =>{
+            onCharlistLoading();
+            marvelService.getAllCharacters(offset)
+            .then(onCharListLoaded)
+            .catch(onError)
+    }
+
+    const onCharlistLoading = () => {
+        setNewItemLoading(true)
     }
     
-    marvelService = new MarvelService();
-
-    componentDidMount() {
-        this.onRequest()
-    }
-
-
-    onRequest = (offset) =>{
-        this.onCharlistLoading();
-        this.marvelService.getAllCharacters(offset)
-            .then(this.onCharListLoaded)
-            .catch(this.onError)
-    }
-
-    onCharlistLoading = () => {
-        this.setState({
-            newItemLoading: true
-        })
-    }
-    
-    onCharListLoaded = (newCharList) => {
+    const onCharListLoaded = (newCharList) => {
         let ended = false;
         if (newCharList.length < 9) {
             ended = true;
         }
-        this.setState(({offset,charList}) => ({
-            charList: [...charList,...newCharList],
-            loading: false,
-            newItemLoading: false,
-            offset: offset+9,
-            charEnded : ended
-        }))
+        setCharList(charList => [...charList,...newCharList])
+        setLoading(loading => false)
+        setNewItemLoading(newItemLoading => false)
+        setOffset(offset => offset+9)
+        setCharEnded(charEnded => ended)
+
     }
 
-    onError = () => {
-        this.setState({
-            error: true,
-            loading: false
-        })
+    const onError = () => {
+        setError(true)
+        setLoading(loading => false)
     }
 
-    itemRefs = [];
-
-    setRef = (ref) => {
-        this.itemRefs.push(ref);
-    }
+    const itemRefs = useRef([]);
     
-    focusOnItem = (id) => {
+    const focusOnItem = (id) => {
         // Я реалізував варіант трохи складніший, з класом і фокусом.
         // Однак теоретично можна залишити лише фокус, і використовувати його в стилях замість класу.
         // Насправді, рішення з CSS-класом можна зробити, витягнувши персонажа
         // у окремий компонент. Але код буде більший, з'явиться новий стан
         // і не факт, що ми виграємо в оптимізації за рахунок більшої кількості елементів.
         // У разі можливості, не зловживайте рефами, використовуйте їх тільки у крайніх випадках.
-        this.itemRefs.forEach(item => item.classList.remove('char__item_selected'));
-        this.itemRefs[id].classList.add('char__item_selected');
-        this.itemRefs[id].focus();
-        console.log(this.itemRefs[this.itemRefs.length - 1])
+        itemRefs.current.forEach(item => item.classList.remove('char__item_selected'));
+        itemRefs.current[id].classList.add('char__item_selected');
+        // itemRefs.current[id].focus();
     }
 
     // this method create for optimization
     // to not put this contruction in render
-    renderItems(arr) {
+    function renderItems(arr) {
         const items =  arr.map((item,i) => {
             const isWithoutImg = item.thumbnail.includes("image_not_available")
             const letChangeObjectFit = isWithoutImg? {objectFit: 'fill'} : null
             
             return (
                 <li 
-                    ref={this.setRef}
+                    ref={el=>itemRefs.current[i] = el}
                     className="char__item"
                     key={item.id}
                     onClick={() => {
-                        this.props.onCharSelected(item.id);
-                        this.focusOnItem(i);
+                        props.onCharSelected(item.id);
+                        focusOnItem(i);
                     }}
                     tabIndex={0}
                     onKeyPress={(e) => {
                         e.preventDefault()
                         if (e.key === ' ' || e.key === "Enter") {
-                            this.props.onCharSelected(item.id);
-                            this.focusOnItem(i);
+                            props.onCharSelected(item.id);
+                            focusOnItem(i);
                         }
                     }}
                     >
@@ -112,10 +103,9 @@ class CharList extends Component {
         )
     }
 
-    render() {
+    
 
-        const {charList, loading, error,offset,newItemLoading,charEnded} = this.state;
-        const items = this.renderItems(charList);
+        const items = renderItems(charList);
 
         const errorMessage = error ? <ErrorMessage/> : null;
         const spinner = loading ? <Spinner/> : null;
@@ -130,14 +120,14 @@ class CharList extends Component {
                 className="button button__main button__long"
                 disabled={newItemLoading}
                 style={{display : charEnded? 'none' : 'block'}}
-                onClick={()=> this.onRequest(offset)}
+                onClick={()=> onRequest(offset)}
                 
                 >
                     <div className="inner">load more</div>
                 </button>
             </div>
         )
-    }
+    
 }
 
 export default CharList;
