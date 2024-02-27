04c9bdc0372bf50f211e02cfac7ca039ede7095d
Razdor13 <oneamaday@gmail.com>
2024-02-27 18:34:57 +0200

    worked with routing and bibl react router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,40 +1,25 @@
-import { useState } from 'react';
-import { BrowserRouter as Router,Route , Switch } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
+
+import {MainPage, ComicsPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import ErrorBoundary from '../errorBoundary/ErrorBoundary';
-import ComicsList from '../comicsList/ComicsList';
-import decoration from '../../resources/img/vision.png';
-import AppBanner from '../appBanner/AppBanner';
 
 const App = () => {
-    const [selectedChar,setChar] = useState(null)
-    const onCharSelected = (id) => {
-        setChar(id)
-    }
+    
     return (
         <Router>
-            <Switch>
-                <div className="app">
-                    <AppHeader/>
-                    <Route exact path = '/'>
-                        <main>
-                            <RandomChar/>
-                            <div className="char__content">
-                                <CharList onCharSelected={onCharSelected}/>
-                                <ErrorBoundary><CharInfo charId={selectedChar}/></ErrorBoundary>
-                            </div>
-                            <img className="bg-decoration" src={decoration} alt="vision"/>
-                        </main>
-                    </Route>
-                    <Route exact path = '/comics'>
-                        <AppBanner/>
-                        <ComicsList/>
-                    </Route>
-                </div>
-            </Switch>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    <Switch>
+                        <Route exact path="/">
+                            <MainPage/>
+                        </Route>
+                        <Route exact path="/comics">
+                            <ComicsPage/>
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
         </Router>
     )
 }
