16c57b132843325c85e9736c57688f8e989a3cf3
Razdor13 <oneamaday@gmail.com>
2024-02-29 16:38:29 +0200

    worked with dinamic import (react.lazy)

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,8 +1,14 @@
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-
-import {MainPage, ComicsPage,Page404,SingleComicPage} from '../pages';
+import { lazy , Suspense} from 'react';
+// import {MainPage, ComicsPage,SingleComicPage} from '../pages';
 import AppHeader from "../appHeader/AppHeader";
+import Spinner from '../spinner/Spinner';
 
+//всі динамічні імпорти треба робити після статичних 
+const Page404 = lazy(() => import('../pages/404'))
+const MainPage = lazy(() => import('../pages/MainPage'))
+const ComicsPage = lazy(() => import('../pages/ComicsPage'))
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'))
 const App = () => {
     
     return (
@@ -10,12 +16,15 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route  path="/" element = {<MainPage/>}/>  
-                        <Route  path="/comics" element = {<ComicsPage/>}/>
-                        <Route  path="/comics/:comicId" element = {<SingleComicPage/>}/>
-                        <Route path="*" element={<Page404/>}/>
-                    </Routes>
+                    <Suspense fallback = {<Spinner/>}>
+                        {/* обгортка що необхідна для того щоб показати щось у випадку якщо динамічний експорт ще не підгрузився */}
+                        <Routes>
+                            <Route  path="/" element = {<MainPage/>}/>  
+                            <Route  path="/comics" element = {<ComicsPage/>}/>
+                            <Route  path="/comics/:comicId" element = {<SingleComicPage/>}/>
+                            <Route path="*" element={<Page404/>}/>
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
