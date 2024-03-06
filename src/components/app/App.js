837501b1b02df37c300d51910990c96a772e4992
Razdor13 <oneamaday@gmail.com>
2024-03-06 18:35:50 +0200

    fix route

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,33 +1,32 @@
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
-import { lazy , Suspense} from 'react';
-import AppHeader from "../appHeader/AppHeader";
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { lazy, Suspense } from 'react';
+import AppHeader from '../appHeader/AppHeader';
 import Spinner from '../spinner/Spinner';
 
-//всі динамічні імпорти треба робити після статичних 
-const Page404 = lazy(() => import('../pages/404'))
-const MainPage = lazy(() => import('../pages/MainPage'))
-const ComicsPage = lazy(() => import('../pages/ComicsPage'))
-const SingleComicPage = lazy(() => import('../pages/SingleComicPage'))
+// Всі динамічні імпорти треба робити після статичних
+const Page404 = lazy(() => import('../pages/404'));
+const MainPage = lazy(() => import('../pages/MainPage'));
+const ComicsPage = lazy(() => import('../pages/ComicsPage'));
+const SingleComicPage = lazy(() => import('../pages/SingleComicPage'));
+
 const App = () => {
-    
     return (
-        <Router>
+        <Router basename="/ReactMarvelWithApi">
             <div className="app">
-                <AppHeader/>
+                <AppHeader />
                 <main>
-                    <Suspense fallback = {<Spinner/>}>
-                        {/* обгортка що необхідна для того щоб показати щось у випадку якщо динамічний експорт ще не підгрузився */}
+                    <Suspense fallback={<Spinner />}>
                         <Routes>
-                            <Route  path="/" element = {<MainPage/>}/>  
-                            <Route  path="/comics" element = {<ComicsPage/>}/>
-                            <Route  path="/comics/:comicId" element = {<SingleComicPage/>}/>
-                            <Route path="*" element={<Page404/>}/>
+                            <Route path="/" element={<MainPage />} />
+                            <Route path="/comics" element={<ComicsPage />} />
+                            <Route path="/comics/:comicId" element={<SingleComicPage />} />
+                            <Route path="*" element={<Page404 />} />
                         </Routes>
                     </Suspense>
                 </main>
             </div>
         </Router>
-    )
-}
+    );
+};
 
 export default App;
