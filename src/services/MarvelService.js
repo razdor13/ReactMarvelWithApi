8f02b41fd850391b6f19c1e74f47497ef17da049
razdor13 <oneamaday@gmail.com>
2025-08-04 19:01:44 +0200

    fix

--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -1,59 +1,72 @@
 import { useHttp } from "../hooks/http.hooks"
 
 
-const  useMarvelService = () => {
+const useMarvelService = () => {
+	const {loading, request, error, clearError} = useHttp();
 
-    const {loading,request,error,clearError} = useHttp()
+	// const _apiBase = 'https://gateway.marvel.com:443/v1/public/';
+	// const _apikey = 'apikey=10877f709e567804be49abd6a4c6cbcd';
+	// const _baseOffset = 210;
 
+	const _apiBase = 'https://marvel-server-zeta.vercel.app/';
+	const _apikey = 'apikey=d4eecb0c66dedbfae4eab45d312fc1df';
+	const _baseOffset = 0;
 
-    const _apiBase = 'https://gateway.marvel.com:443/v1/public/'
-    const _apiKey = 'apikey=64edc5943517781106564a6ac5e58cbd'
-    const _baseOffset = 210
-    
-    const getAllCharacters = async (offset = _baseOffset) => {
-        const res = await request(`${_apiBase}characters?limit=9&offset=${offset}&${_apiKey}`);
-        return res.data.results.map(_transformCharacter);
-    }
+	const getAllComics = async(offset = _baseOffset) => {
+		const res = await request(
+			`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apikey}`
+		);
+		return res.data.results.map(_transformComics);
+	}
 
-    const getCharacter = async (id) => {
-        const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
-        return _transformCharacter(res.data.results[0]);
-    }
-    const getCharacterByName = async (name) => {
-		const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+	const getAllCharacters = async(offset = _baseOffset) => {
+		const res = await request(
+			`${_apiBase}characters?limit=9&offset=${offset}&${_apikey}`
+		);
 		return res.data.results.map(_transformCharacter);
-	};
-    const getComics = async (offset=0) => {
-        const res = await request(
-			`${_apiBase}comics?orderBy=issueNumber&limit=8&offset=${offset}&${_apiKey}`)
-        return res.data.results.map(_transformComics)
-    }
-    
-    const getComic = async (id) => {
-		const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+	}
 
+	const getCharacterByName = async(name) => {
+		const res = await request (
+			`${_apiBase}characters?name=${name}&${_apikey}`
+		);
+		return res.data.results.map(_transformCharacter);
+	}
+
+	const getCharacter = async (id) => {
+		const res =  await request(
+			`${_apiBase}characters/${id}?${_apikey}`
+		);
+		return _transformCharacter(res.data.results[0]);
+	}
+
+	const getComic = async (id) => {
+		const res = await request(
+			`${_apiBase}comics/${id}?${_apikey}`
+		)
 		return _transformComics(res.data.results[0]);
-	};
+	}
 
-    const getComicsInfo = async (id) => {
-		const res = await request(`${_apiBase}comics/${id}?${_apiKey}`);
+	const _transformCharacter = (char) => {
+		// let str = char.description;
+		// if (str.length > 100) {
+		// 	str = str.slice(0, 220)+"...";
+		// }
+		// if (str.length === 0) {
+		// 	str = 'Description is not available for this character'
+		// }
+		return {
+			id: char.id,
+			name: char.name,
+			description: char.description ? char.description.length > 200 ? char.description.slice (0, 200) +"..." :  char.description:  'Description is not available for this character',
+			thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+			homepage: char.urls[0].url,
+			wiki: char.urls[1].url,
+			comics: char.comics.items
+		}
+	}
 
-		return _transformComics(res.data.results[0]);
-	};
-
-    const _transformCharacter = (char) => {
-        return {
-            id: char.id,
-            name: char.name,
-            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for character',
-            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
-            homepage: char.urls[0].url,
-            wiki: char.urls[1].url,
-            comics: char.comics.items
-        }
-    }
-
-    const _transformComics = (comics) => {
+	const _transformComics = (comics) => {
 		return {
 			id: comics.id,
 			title: comics.title,
@@ -69,8 +82,16 @@ const  useMarvelService = () => {
 				: "not available",
 		};
 	};
-    
-    return  {loading,error,getCharacterByName,getAllCharacters,getCharacter,clearError,getComics,getComicsInfo,getComic}
+	return {
+		loading, 
+		error, 
+		clearError,
+		getAllCharacters, 
+		getCharacter, 
+		getCharacterByName, 
+		getAllComics,
+		getComic
+	}
 }
 
 
